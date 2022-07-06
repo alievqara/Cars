0cd91a5b57c18b1d136bfb54f5921287a79be53d
Aliev Qara <alievqara@outlook.com>
2022-07-06 23:44:39 +0400

    cars

---
+++ Cars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cars
+{
+
+
+    public class Program
+    {
+        static void Main()
+        {
+
+            Sedan bmw = new Sedan("E46", 320, "Green", "15000");
+
+            Console.WriteLine($"{bmw.Model},{bmw.Color},{bmw.topSpeed},{bmw.Price}");
+
+
+
+
+
+            Sedan mersedes = new Sedan("G65", 300, "White", "150000Azn");
+
+            Console.WriteLine($"{mersedes.Model},{mersedes.Color},{mersedes.topSpeed},{mersedes.Price}");
+
+
+        }
+    }
+}
---
+++ Cars/Sedan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Sedan
+    {
+
+        public string Model;
+        public int topSpeed;
+        public string Color;
+        public string Price;
+
+
+
+        public Sedan()
+        {
+            //Console.WriteLine("Default");
+
+        }
+
+        public Sedan(string model, int topSpeed, string color, string price) : this()
+        {
+            Model = model;
+            this.topSpeed = topSpeed;
+            Color = color;
+            Price = price;
+
+
+            //Console.WriteLine("Custom");
+
+        }
+    }
+}
